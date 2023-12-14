7a6d3c55999cd8800e82d3a54334bea3bdc004a7
SVNRiS3 <krzy.driwa@gmail.com>
2023-12-14 13:38:46 +0100

    Create Gameboard and Players

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-const gameboard = (function () {
-	const gameboard = [];
+const Gameboard = (function () {
+	const BOARDSIZE = 3;
+	const board = [];
+
+	for (let i = 0; i < BOARDSIZE; i++) {
+		board[i] = [];
+		for (let j = 0; j < BOARDSIZE; j++) {
+			board[i].push(Cell());
+		}
+	}
+
+	const getBoard = () => board;
+
+	return { getBoard };
+})();
+
+function Cell() {
+	let symbol = "";
+
+	const setSymbol = (newSymbol) => (symbol = newSymbol);
+	const getSymbol = () => symbol;
+
+	return { setSymbol, getSymbol };
+}
+
+function Player(name, symbol) {
+	const getName = () => name;
+	const getSymbol = () => symbol;
+
+	return { getName, getSymbol };
+}
+
+const GameControl = (function () {
+	const firstPlayer = Player("P1", "x");
+	const secondlayer = Player("P2", "o");
 })();
