d1e8070ab72142e541cc30d2c869ef6d61921059
SVNRiS3 <krzy.driwa@gmail.com>
2023-12-21 15:57:21 +0100

    Move symbol to Player object, rename GameControl

--- script.js
+++ script.js
@@ -23,14 +23,14 @@ function Cell() {
 	return { setSymbol, getSymbol };
 }
 
-function Player(name) {
-	const getName = () => name;
+function Player(name, symbol) {
+	const getInfo = () => ({ name, symbol });
 
-	return { getName };
+	return { getInfo };
 }
 
 const renderGameboard = (function () {
-	const consoleRender = (playerName, symbol) => {
+	const consoleRender = (playerInfo) => {
 		console.log("   " + [0, 1, 2].join(" | ") + " ");
 		Gameboard.getBoard().forEach((arr, index) => {
 			console.log(
@@ -42,46 +42,47 @@ const renderGameboard = (function () {
 					"]"
 			);
 		});
-		console.log(playerName + ", where to draw " + symbol + "?");
+		console.log(
+			playerInfo.name +
+				", where to draw " +
+				playerInfo.symbol +
+				"?"
+		);
 	};
 
 	return { consoleRender };
 })();
 
-const GameControl = (function () {
-	let symbol = "x";
+const GameControls = (function () {
 	let playingPlayer = 1;
-	const firstPlayer = Player("Player1");
-	const secondPlayer = Player("Player2");
-	const getPlayerName = (player) => {
-		if (player === 1) {
-			return firstPlayer.getName();
-		} else if (player === 2) {
-			return secondPlayer.getName();
+	const firstPlayer = Player("Player1", "x");
+	const secondPlayer = Player("Player2", "o");
+	const getPlayerInfo = () => {
+		if (playingPlayer === 1) {
+			return firstPlayer.getInfo();
+		} else if (playingPlayer === 2) {
+			return secondPlayer.getInfo();
 		}
 	};
 
 	const isFieldOccupied = (row, column) =>
 		Boolean(Gameboard.getBoard()[row][column].getSymbol());
 
-	const getSymbol = () => symbol;
-	renderGameboard.consoleRender(getPlayerName(playingPlayer), symbol);
+	renderGameboard.consoleRender(getPlayerInfo());
 
 	const playRound = (row, column) => {
 		if (!isFieldOccupied(row, column)) {
-			Gameboard.getBoard()[row][column].setSymbol(symbol);
+			Gameboard.getBoard()[row][column].setSymbol(
+				getPlayerInfo().symbol
+			);
 			switchPlayer();
 		}
-		renderGameboard.consoleRender(
-			getPlayerName(playingPlayer),
-			symbol
-		);
+		renderGameboard.consoleRender(getPlayerInfo());
 	};
 
 	const switchPlayer = () => {
-		symbol = symbol === "x" ? "o" : "x";
 		playingPlayer = playingPlayer === 1 ? 2 : 1;
 	};
 
-	return { playRound, getSymbol };
+	return { playRound };
 })();
